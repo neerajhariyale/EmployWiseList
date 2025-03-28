9f6f17678a9a36ab7723991f25ec82ab1c4f3619
neerajhariyale <hariyaleneeraj31@gmail.com>
2025-03-28 11:56:51 +0530

    six commit

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 // const API_BASE_URL =process.env.REACT_APP_API_BASE_URL || 'https://reqres.in/api';
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ;
+const API_BASE_URL = process.env.BASE_URL ;
 
 
 console.log("API_BASE_URL: ", API_BASE_URL);
