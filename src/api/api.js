b7ce63650eac5e1ce5e2f6d55935436480675fc5
neerajhariyale <hariyaleneeraj31@gmail.com>
2025-03-28 10:59:44 +0530

    thirdd commt

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL =process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL =process.env.REACT_APP_API_BASE_URL || 'https://reqres.in/api';
 
-console.log("API Base URL:", process.env.REACT_APP_API_BASE_URL);
+console.log("process.env.REACT_APP_API_BASE_URL", process.env.REACT_APP_API_BASE_URL);
+console.log("API_BASE_URL: ", API_BASE_URL);
 
 
 export const loginUser = async (email, password) => {
